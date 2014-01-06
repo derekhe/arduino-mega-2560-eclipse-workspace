313618d6e9230c2b37f244fa5a4f36bb0a980fc8
derekhe <derekhe@april1985.com>
2014-01-06 23:14:01 +0800

    Modified LCM1602 Demo. Change input from serial to serial1

--- LCM1602/src/HelloWorld.cpp
+++ LCM1602/src/HelloWorld.cpp
@@ -1,61 +1,54 @@
-#include <Wire.h> 
+#include <Wire.h>
+// Get the LCD I2C Library here:
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
+// Move any other LCD libraries to another folder or delete them
+// See Library "Docs" folder for possible commands etc.
 #include <LiquidCrystal_I2C.h>
 
-
-
-#define BACKLIGHT_PIN     13
-
-LiquidCrystal_I2C lcd(0x38);  // Set the LCD I2C address
-
-//LiquidCrystal_I2C lcd(0x38, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
-
-
-// Creat a set of new characters
-const uint8_t charBitmap[][8] = {
-   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
-   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
-   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
-   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
-   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
-   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
-   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
-   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
-   
-};
+// Set the pins on the I2C chip used for LCD connections:
+//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 void setup()
 {
-   int charBitmapSize = (sizeof(charBitmap ) / sizeof (charBitmap[0]));
+  Serial1.begin(115200);  // Used to type in characters
 
-  // Switch on the backlight
-  pinMode ( BACKLIGHT_PIN, OUTPUT );
-  digitalWrite ( BACKLIGHT_PIN, HIGH );
-  
-  lcd.begin(16,2);               // initialize the lcd 
+  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
 
-   for ( int i = 0; i < charBitmapSize; i++ )
-   {
-      lcd.createChar ( i, (uint8_t *)charBitmap[i] );
-   }
+// ------- Quick 3 blinks of backlight  -------------
+  for(int i = 0; i< 3; i++)
+  {
+    lcd.backlight();
+    delay(250);
+    lcd.noBacklight();
+    delay(250);
+  }
+  lcd.backlight();
 
-  lcd.home ();                   // go home
-  lcd.print("Hello, ARDUINO ");  
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print (" FORUM - fm   ");
-  delay ( 1000 );
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("Derekhe");
+  delay(8000);
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Use Serial Mon");
+  lcd.setCursor(0,1);
+  lcd.print("Type to display");
 }
+
 
 void loop()
 {
-   lcd.home ();
-   // Do a little animation by writing to the same location
-   for ( int i = 0; i < 2; i++ )
-   {
-      for ( int j = 0; j < 16; j++ )
-      {
-         lcd.print (char(random(7)));
+  {
+    if (Serial1.available()) {
+      delay(100);
+
+      while (Serial1.available() > 0) {
+        lcd.write(Serial1.read());
       }
-      lcd.setCursor ( 0, 1 );
-   }
-   delay (200);
+    }
+  }
 }
